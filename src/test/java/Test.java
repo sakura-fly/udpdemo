d081e92c2eb0da9b9d597d7a7e97bb48a9568470
sakura-fly <sourcesakura@sina.com>
2019-12-30 15:05:54 +0800

    添加发布GitHub仓库配置

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -7,22 +7,23 @@ public class Test {
     public static void main(String[] args) throws IOException {
         byte[] data = new byte[]{(byte) 0xBB, (byte) 0x00, (byte) 0x0E, (byte) 0xB5, (byte) 0x02, (byte) 0x10, (byte) 0x11, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x03, (byte) 0x66};
 
-        UdpClient client = new UdpClient("192.168.2.253", 1030, new ReceiveListener() {
-            @Override
-            public void receive(byte[] msg) {
-                for (byte b : msg) {
-                    System.out.println(String.format("0x%02x", b));
-                }
-            }
-
-            @Override
-            public void err(Exception e) {
-                System.out.println("err");
-            }
-        });
+//        UdpClient client = new UdpClient("192.168.2.253", 1030, new ReceiveListener() {
+//            @Override
+//            public void receive(byte[] msg) {
+//                for (byte b : msg) {
+//                    System.out.println(String.format("0x%02x", b));
+//                }
+//            }
+//
+//            @Override
+//            public void err(Exception e) {
+//                System.out.println("err");
+//            }
+//        });
+        UdpClient client = new UdpClient("192.168.2.253", 1030);
         client.connect();
         long time1 = System.currentTimeMillis();
-        System.out.println(client.send(data));
+        System.out.println(client.send(data).length);
         client.close();
         long time2 = System.currentTimeMillis();
         System.out.println(time2 - time1);
